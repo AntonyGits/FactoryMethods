4abd3f2b9d65134e0873d3c12fe70cd52a2aedb9
Anthony Plotnikov <plotnikof.a@gmail.com>
2017-11-07 16:05:15 +0300

    Patters OOP FectoryMethod

---
+++ FactoryMethod.Tests/CommandTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FactoryMethod.Tests
+{
+    [TestClass]
+    public class CommandTests
+    {
+        [TestMethod]
+        public void VolumeTest()
+        {
+            var volume = new AquosCommand("VOLM", 30);
+            var volume2 = AquosCommand.Volume(30);
+
+            Assert.IsTrue(volume.ToString() == volume2.ToString());
+        }
+
+        [TestMethod]
+        public void PowerTest()
+        {
+            var povwer = new AquosCommand("POWR", 0);
+            var power2 = AquosCommand.Power(PowerSetting.Off);
+
+            Assert.IsTrue(povwer.ToString() == power2.ToString());
+        }
+    }
+}
---
+++ FactoryMethod/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var volume = new AquosCommand("VOLM", 30);
+            
+        }
+    }
+}
